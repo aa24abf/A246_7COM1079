1d065d937d6640d8808fa9a95ccd20313794a928
Vishnuvardhan Parasuraman <vp24aau@herts.ac.uk>
2024-11-28 22:19:56 +0000

    Changed the code for SMA function, got success.

--- SMA_Calculation.R
+++ SMA_Calculation.R
@@ -2,9 +2,10 @@ library(TTR)
 data = read.csv("005380.KS.csv")
 data
 
-data$sma = SMA(data$close, n = 20)
+data$sma = SMA(data$Close, n = 20)
 data$diff = data$High - data$Low
 data
+
 write.csv(data, "Dataset_SMA.csv", row.names = FALSE)
 
 
