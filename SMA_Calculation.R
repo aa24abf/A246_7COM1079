8ffe11297e0c454e8e935025818172f05701dce0
Yesudian <jy23aau@herts.ac.uk>
2025-01-06 18:30:33 +0000

    Added  comments

--- SMA_Calculation.R
+++ SMA_Calculation.R
@@ -6,7 +6,7 @@ data$sma = SMA(data$Close, n = 20) # Compute 20-period SMA of 'Close' and store 
 data$diff = data$High - data$Low # Compute 'High' - 'Low' and store in 'diff' column
 data
 
-write.csv(data, "Dataset_SMA.csv", row.names = FALSE)
+write.csv(data, "Dataset_SMA.csv", row.names = FALSE) # Save 'data' to 'Dataset_SMA.csv' without row names
 
 
 
