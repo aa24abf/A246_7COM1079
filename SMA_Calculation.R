0434ceb64d292530085f2cd11925810fc4b52dea
Yesudian <jy23aau@herts.ac.uk>
2025-01-06 18:28:39 +0000

    Added the comments

--- SMA_Calculation.R
+++ SMA_Calculation.R
@@ -3,7 +3,7 @@ data = read.csv("005380.KS.csv")
 data
 
 data$sma = SMA(data$Close, n = 20) # Compute 20-period SMA of 'Close' and store it in 'sma' column
-data$diff = data$High - data$Low
+data$diff = data$High - data$Low # Compute 'High' - 'Low' and store in 'diff' column
 data
 
 write.csv(data, "Dataset_SMA.csv", row.names = FALSE)
