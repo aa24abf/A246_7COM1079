13c4e6004df110624c9c4fb90e0e7fbee954c38b
Abinaya Sri Arunkumar <aa24abf@herts.ac.uk>
2025-01-07 08:57:24 +0000

    Finalizing Data Visualization Code - Abinaya

--- Data_Visualization.R
+++ Data_Visualization.R
@@ -6,38 +6,42 @@ data
 
 summary(data)
 
+# Data Cleaning - Removing Null values
+
 sum(!is.finite(data$sma))
 data_cleaned <- data[!is.na(data$sma) & !is.nan(data$sma) & !is.infinite(data$sma), ]
 sum(!is.finite(data_cleaned$sma))
 
+y <- data_cleaned$pricevolatility
+x <- data_cleaned$sma
 
+# Histogram of Price Volatility
 
-# Histogram of Difference btw High and Low and 20-day SMA of Hyundai Stock
-h <- hist(data_cleaned$diff
-  
+h <- hist(y
   , 12
-  , main = "Hyundai Stock Frequency"
-  , xlab = "Difference of High and Low"
+  , main = "Histogram of Price Volatility"
+  , xlab = "Price Volatility (in KRW)"
   , ylab = "Frequency"
   , col  = "blue"
   )
-xfit <- seq(min(data_cleaned$diff), max(data_cleaned$diff), length = 40) 
-yfit <- dnorm(xfit, mean = mean(data_cleaned$diff), sd = sd(data_cleaned$diff)) 
-yfit <- yfit * diff(h$mids[1:2]) * length(data_cleaned$diff) 
+xfit <- seq(min(y), max(y), length = 40) 
+yfit <- dnorm(xfit, mean = mean(y), sd = sd(y)) 
+yfit <- yfit * diff(h$mids[1:2]) * length(y) 
 
 lines(xfit, yfit, col = "black", lwd = 2)
 
+# Scatterplot of Price Volatility vs. 20-Day Simple Moving Average
 
-y <- data_cleaned$diff
-x <- data_cleaned$sma
-
-
-plot(x,y,main = "Hyundai Stock - ScatterPlot" , xlab = "Difference btw High and Low" , ylab = "20-day SMA" , pch = 19, frame = T)
+plot(x,y,main = "Scatterplot of Price Volatility vs. 20-Day Simple Moving Average" , xlab = "Price Volatility (in KRW)" , ylab = "20-day SMA (in KRW)" , pch = 19, frame = T)
 model <- lm(y ~ x, data = data_cleaned)
 abline(model, col = "blue")
 
+# Kendall Statistical Test
+
 cor_test_result <- cor.test(x, y, method = "kendall")
 print(cor_test_result)
+
+# Spearman Statistical Test
 
 cor_test_result <- cor.test(x, y, method = "spearman")
 print(cor_test_result)
